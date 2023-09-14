b595c254279d0ee63e57d8dd4f1ad6fb6bb041fc
reidwatson <65178364+reidwatson@users.noreply.github.com>
2023-09-14 18:38:47 -0400

    add ability to search terms
    text input to filter the data based on your search term

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,13 @@ import './Home.css';
 
 const Home: React.FC = () => {
 
+  //react hook variables
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+
+  //column definitions for the data table
   const columns = [
     {
       name: 'Address',
@@ -55,9 +62,8 @@ const Home: React.FC = () => {
     }
   ];
 
-  const [data, setData] = useState([]);
 
-
+  //initialization function: get the xml data from the local path, parse to json, set json data as react hook variable.
   useEffect(() => {
     fetch('/xml/ab.xml')
       .then(response => response.text())
@@ -65,8 +71,8 @@ const Home: React.FC = () => {
         console.log(data);
         const parser = new XMLParser();
         let jsonData = parser.parse(data);
-        console.log(jsonData);
         setData(jsonData.AddressBook.Contact);
+        setFilteredData(jsonData.AddressBook.Contact);
       })
       .catch(error => {
         console.error('Error fetching the XML:', error);
@@ -75,21 +81,55 @@ const Home: React.FC = () => {
 
 
 
+  //scrub the names of all accent marks so search terms work on accented characters too
+  //(I got this regex from chatgpt -- not gonna lie)
+  function scrub(name: string) {
+    return name.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toUpperCase();
+}
+
+  //filter the data being shown whenever the search term gets updated (whenever you type in the input field)
+  //if the input field is blank or there is an error, default back to the whole data set.
+  useEffect(() => {
+    try { 
+      console.log('searching...', searchTerm)
+      let result = [...data];
+
+      //if there is a search term, filter the data by this term
+      //make a long string, upper cased, no accents, for every object; then see if this string includes the search term
+      if (searchTerm !== '') {
+        let scrubbedTerm = scrub(searchTerm);
+        result = result.filter((x: any) => scrub(`${x.Address} ${x.City} ${x.CompanyName} ${x.ContactName} ${x.ContactTitle} ${x.Country} ${x.CustomerID} ${x.Email} ${x.Fax} ${x.Phone} ${x.PostalCode}`).includes(scrubbedTerm));
+      }
+
+      setFilteredData(result);
+    } catch (e) {
+      console.error('Error trying to filter data: ', e)
+      setFilteredData(data);
+    }
+  }, [searchTerm]);//listen to changes on 'searchTerm'
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Address Book</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Address Book</IonTitle>
           </IonToolbar>
         </IonHeader>
 
-        <DataTable data={data} columns={columns} responsive pagination />
+        <section className='searchWrapper'>
+          <label>Search by any field</label>
+          <input type='text' value={searchTerm} onInput={(e: any) => setSearchTerm(e.target.value)} placeholder='Search Term'/>
+        </section>
+
+        <section className='tableWrapper'>
+          <DataTable data={filteredData} columns={columns} responsive pagination />
+        </section>
 
         {/* <ExploreContainer /> */}
       </IonContent>
